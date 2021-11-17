47fe27978f4c082b14e18d4cf26f9cf84b843e76
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-11-17 20:33:15 +0100

    Delete src directory

--- src/components/ResetGameButton/ResetGameButton.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ResetGameButton = props => (
-  <input type="button" value="reset" onClick={props.click} />
-);
-
-export default ResetGameButton;
--- src/components/WronglyGuessedLetters/WronglyGuessedLetters.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const WronglyGuessedLetters = props => (
-  <div>{props.wrongLetters.join(" ")}</div>
-);
-
-export default WronglyGuessedLetters;
